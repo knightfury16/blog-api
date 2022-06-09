58e9ee824c37de1e0bf2350f6bdeea49b257959d
knightfury16 <suhaibknight@gmail.com>
2022-06-09 17:07:28 +0600

    Logout from current device done.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { validateOrReject } from 'class-validator';
 import express, { Request, Response } from 'express';
 import './db/sqlConnection';
+import { Token } from './entity/Token';
 import { User } from './entity/User';
 import auth from './middleware/auth';
 
@@ -31,6 +32,15 @@ app.post('/users/login', async (req: Request, res: Response) => {
   } catch (error: any) {
     console.log(error);
     res.status(400).send();
+  }
+});
+
+app.post('/users/logout', auth, async (req: Request, res: Response) => {
+  try {
+    await Token.delete({ ownerId: req.user.id, token: req.token });
+    res.send('Logout');
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
