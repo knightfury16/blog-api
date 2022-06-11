cf49f4ab0ac8e34faba9ee5d9275f874de0fdb3f
knightfury16 <suhaibknight@gmail.com>
2022-06-11 23:16:07 +0600

    Get blog route added with pagignation.

--- src/routers/blog.ts
+++ src/routers/blog.ts
@@ -5,7 +5,7 @@ import auth from '../middleware/auth';
 
 const router = express.Router();
 
-router.post('/blog/create', auth, async (req: Request, res: Response) => {
+router.post('/blogs/create', auth, async (req: Request, res: Response) => {
   const blog = Blog.create({
     ...req.body,
     ownerId: req.user.id
@@ -14,6 +14,22 @@ router.post('/blog/create', auth, async (req: Request, res: Response) => {
     await validateOrReject(blog);
     await blog.save();
     res.status(201).send(blog);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/blogs', async (req: Request, res: Response) => {
+  const limit = req.query.limit ? parseInt(req.query.limit as string) : undefined;
+  const skip = req.query.skip ? parseInt(req.query.skip as string) : undefined;
+
+  try {
+    const blogs = await Blog.createQueryBuilder('blog')
+      .orderBy('blog.id', 'DESC')
+      .skip(skip)
+      .take(limit)
+      .getMany();
+    res.send(blogs);
   } catch (error) {
     res.status(400).send(error);
   }
